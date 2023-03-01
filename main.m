9557b4823f743bf9655bba577f8cfd5f1124a227
radiobenzene <aurangabadkaruditangshu@gmail.com>
2023-03-02 01:49:00 +0300

    feat: add composite image

--- main.m
+++ main.m
@@ -17,5 +17,26 @@ tic;
 % Performing Bayesian Matting here
 [Fground, Bground, alpha_val] = getBayesianMatte(img, trimap, img_obj);
 
+% Generate composite image here
+composite_img = generateCompositeImage(Fground, Bground, alpha_val);
 % Ending timer here
 toc;
+
+% Displaying Original Image
+figure(1);
+imshow("Images\input-small.png");
+title('Original Image');
+ 
+% Displaying Alpha Matte
+figure(2);
+imshow(alpha_val);
+title('Alpha Matte');
+shg; hold on;
+
+figure(3);
+imshow(composite_img);
+title('Composite Image');
+
+
+
+
