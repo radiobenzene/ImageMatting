bd82036c7185c8baaea8704d5962c82563071f6f
radiobenzene <aurangabadkaruditangshu@gmail.com>
2023-03-02 01:05:11 +0300

    Add: Class for variable ini

--- initializeVariable.m
+++ initializeVariable.m
@@ -5,5 +5,6 @@ classdef initializeVariable
         sigma = 50; % Variance for Gaussian
         cam_sigma = 0.05; % Camera Variance
         min_N = 10; % Minimum Window size
+        clustering_variance = 0.90;
     end
 end
