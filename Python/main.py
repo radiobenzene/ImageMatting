5b172b2913e783bcd107088524dd678c30f6203a
radiobenzene <aurangabadkaruditangshu@gmail.com>
2023-04-01 13:24:37 +0100

    update: add minor fixees

--- Python/main.py
+++ Python/main.py
@@ -6,11 +6,14 @@ import matplotlib.pyplot as plt
 import os
 import sys
 from tqdm import tqdm
+from time import sleep
 
 n = len(sys.argv)
 image_name = sys.argv[1]
 
-#for i in tqdm(range(101), desc="Generating Matte", ascii=False, ncols=75):
+#for i in tqdm(range(100), desc="Generating Matte", ascii=False, ncols=75):
 main(image_name)
+    #sleep(0.01)
+    
 
 #print("Done!")
