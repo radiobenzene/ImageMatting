899ea7471cdc448725f50781243e37b690105aba
radiobenzene <aurangabadkaruditangshu@gmail.com>
2023-04-05 22:36:25 +0100

    update: major updates

--- Python/engine.py
+++ Python/engine.py
@@ -18,10 +18,10 @@ if __name__ == "__main__":
     argument_list = sys.argv[1:]
     
     # Condensed options
-    options = "rmsthu"
+    options = "rmsthp"
     
     # Creating a dictionary of options
-    long_options = ["run", "mse", "sad", "time", "help", "unit"]
+    long_options = ["run", "mse", "sad", "time", "help", "psnr"]
     
     try:
         # Parsing argument
@@ -33,9 +33,25 @@ if __name__ == "__main__":
                 break
             elif currentArgument in ("-s", "--sad"):
                 print("Showing SAD values")
-                displaySAD('SAD.txt')
-                
-    
+                displayMetric('SAD.txt', 'SAD Values', 'Image Name', 'SAD Values')
+            elif currentArgument in ("-m", "--mse"):
+                print("Showing MSE values")
+                displayMetric('mse.txt', 'MSE', 'Image Name', 'MSE Value')
+            elif currentArgument in ("-t", "--time"):
+                print("Showing Execution Times")
+                displayMetric('execution.txt', 'Execution Time', 'Image Name', 'Execution Time (in seconds)')
+            elif currentArgument in ("-p", "--psnr"):
+                print("Showing PSNR")
+                displayMetric('psnr.txt', 'PSNR', 'Image Name', 'PSNR')
+            elif currentArgument in ("-h", "--help"):
+                print("Showing Help")
+                print("-h or --help for help")
+                print("-r or --run to run")
+                print("-m or --mse to display MSE")
+                print("-s or --sad to display SAD")
+                print("-p or --psnr to display PSNR")
+                print("-t or --time to display execution time")
+
     except getopt.err as error:
         err = "Invalid Option"
         print(str(err))
