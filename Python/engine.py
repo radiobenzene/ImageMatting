d6534d15fb5627cec9030a9d80ce0b8997815130
radiobenzene <aurangabadkaruditangshu@gmail.com>
2023-04-05 15:04:54 +0100

    feat: add command line selector

--- Python/engine.py
+++ Python/engine.py
@@ -6,16 +6,39 @@ import matplotlib.pyplot as plt
 import os
 import sys
 from tqdm import tqdm
-
+import getopt
+import testFlow
+from metrics import *
 # Main Engine
 
 
 # n = len(sys.argv)
 # image_name = sys.argv[1]
+if __name__ == "__main__":
+    argument_list = sys.argv[1:]
+    
+    # Condensed options
+    options = "rmsthu"
+    
+    # Creating a dictionary of options
+    long_options = ["run", "mse", "sad", "time", "help", "unit"]
+    
+    try:
+        # Parsing argument
+        arguments, values = getopt.getopt(argument_list, options, long_options)
+        for currentArgument, currentValue in arguments:
+            if currentArgument in ("-r", "--run"):
+                print("Running the application")
+                main(user_selector = True, save = True, show_FG = True, compositing= True)
+                break
+            elif currentArgument in ("-s", "--sad"):
+                print("Showing SAD values")
+                displaySAD('SAD.txt')
+                
+    
+    except getopt.err as error:
+        err = "Invalid Option"
+        print(str(err))
+    
 
-
-# main(img_name = "GT01", user_selector = False, save = False, show_FG = False, compositing = False)
-main(user_selector = True, save = True, show_FG = True, compositing= True)
-# main(user_selector = True, save = True, show_FG = True, compositing= True)
-
-# execution_longSeq()
+#main(user_selector = True, save = True, show_FG = True, compositing= True)
