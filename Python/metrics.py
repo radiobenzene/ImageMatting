1de379acb0036aa8a5a15c0331fb874d3ad48288
radiobenzene <aurangabadkaruditangshu@gmail.com>
2023-04-05 22:37:35 +0100

    update: add function to display all metrics

--- Python/metrics.py
+++ Python/metrics.py
@@ -63,6 +63,33 @@ def displayExecutionTime(file_path):
     generateGraph(file_path, 'Execution Time', 'Execution Time')
 
     sys.exit(app.exec_())
+'''
+    Function to display metric
+'''
+def displayMetric(file_path, title, xlabel, ylabel):
+    with open(file_path, 'r') as f:
+        data = [line.strip().split(',') for line in f]
+
+    app = QApplication(sys.argv)
+    table = QTableWidget()
+    table.setRowCount(len(data))
+    table.setColumnCount(2)
+    #table.setHorizontalHeaderLabels(['Image Name', 'Execution Time '])
+    table.setHorizontalHeaderLabels([xlabel, ylabel])
+    for i, row in enumerate(data):
+        table.setItem(i, 0, QTableWidgetItem(row[0]))
+        table.setItem(i, 1, QTableWidgetItem(row[1]))
+
+    layout = QVBoxLayout()
+    layout.addWidget(table)
+
+    widget = QWidget()
+    widget.setLayout(layout)
+    widget.show()
+    
+    generateGraph(file_path, title, ylabel)
+
+    sys.exit(app.exec_())
     
 '''
     Function to generate graph
